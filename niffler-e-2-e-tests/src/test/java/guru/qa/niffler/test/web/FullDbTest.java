b8c54619c5efd3b51e69ca7191e31060f0b79b9d
filkot <filkot@gmail.com>
2025-02-15 18:06:23 +0300

    HW 8.2 done

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/FullDbTest.java
@@ -0,0 +1,23 @@
+package guru.qa.niffler.test.web;
+
+import guru.qa.niffler.model.rest.UserJson;
+import guru.qa.niffler.service.impl.UsersApiClient;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Isolated;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+@Isolated
+public class FullDbTest {
+
+    private final UsersApiClient usersApiClient = new UsersApiClient();
+
+
+    @Test
+    void checkNotEmptyUsersList() {
+        List<UserJson> users = usersApiClient.getAllUsers("filkot");
+        assertFalse(users.isEmpty());
+    }
+}
